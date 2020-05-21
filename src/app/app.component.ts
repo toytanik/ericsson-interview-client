3bc2f53189c5e9926949226859279e1e0661936d
tuncay turk <tuncayturk.cse@gmail.com>
2020-05-21 23:13:58 +0100

    --inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocationService} from './location.service';
+import {Location} from './location';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'location-client';
+  message: any;
+
+  location: Location = new Location('', '', 0);
+  constructor(private service: LocationService) {
+  }
+  ngOnInit() {
+  }
+
+  public registerLocationNow(){
+  const response = this.service.createLocation(this.location);
+  response.subscribe((data) => this.message = data);
+
+  }
 }
---
+++ src/app/location.ts
@@ -0,0 +1,7 @@
+export class Location{
+  constructor (
+      longitude: string,
+      latitude: string,
+      radius: number
+  ){}
+}
